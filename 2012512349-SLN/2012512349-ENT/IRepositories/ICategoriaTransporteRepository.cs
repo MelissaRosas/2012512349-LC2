02357abe53ee7994a1cd9eac446e7ab0e0b167e4
Melissa Rosas <melissa_rosas@usmp.pe>
2017-06-10 17:28:11 -0500

    ACTUALIZACION SEGUNDO COMMIT

--- 2012512349-SLN/2012512349-ENT/IRepositories/ICategoriaTransporteRepository.cs
+++ 2012512349-SLN/2012512349-ENT/IRepositories/ICategoriaTransporteRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2012512349_ENT.IRepositories
 {
-   public interface ICategoriaTransporteRepository
+   public interface ICategoriaTransporteRepository : IRepository
     {
     }
 }
--- 2012512349-SLN/2012512349-ENT/IRepositories/IEmpleadoRepository.cs
+++ 2012512349-SLN/2012512349-ENT/IRepositories/IEmpleadoRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2012512349_ENT.IRepositories
 {
-   public interface IEmpleadoRepository 
+   public interface IEmpleadoRepository : IRepository
     {
     }
 }
